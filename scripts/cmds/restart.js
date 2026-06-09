541ef02bb457e2bba7aeb9f3fc10a1aaff9d67a5
Ismael03-Dev <backupff19@gmail.com>
2026-06-09 20:04:33 +0100

    🦔 Commit: pushall restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -3,15 +3,11 @@ const fs = require("fs-extra");
 module.exports = {
 	config: {
 		name: "restart",
-		version: "1.0",
+		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
 		role: 2,
-		shortDescription: {
-			vi: "Khởi động lại bot",
-			en: "Restart bot"
-		},
-		longDescription: {
+		description: {
 			vi: "Khởi động lại bot",
 			en: "Restart bot"
 		},
@@ -27,7 +23,7 @@ module.exports = {
 			restartting: "🔄 | Đang khởi động lại bot..."
 		},
 		en: {
-			restartting: "🔑 | 𝑅𝑒𝑠𝑡𝑎𝑟𝑡𝑖𝑛𝑔 𝐵𝑜𝑡 (҂`_´)...□□□□□ 0%✨"
+			restartting: "🔄 | Restarting bot..."
 		}
 	},
 
@@ -35,7 +31,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`🍀 | 𝐁𝐨𝐭 𝐫𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\■■■■ 100%(⁠｡⁠•̀⁠ᴗ⁠-⁠)⁠✧🕒 | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
