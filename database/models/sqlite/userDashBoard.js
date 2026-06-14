c4ab9d7934312b1324489b3192638383ca13318f
Ismael03-Dev <backupff19@gmail.com>
2026-06-14 16:10:05 +0200

    🦔 Scan: database/models/sqlite/userDashBoard.js

--- database/models/sqlite/userDashBoard.js
+++ database/models/sqlite/userDashBoard.js
@@ -1,23 +1,37 @@
 const { Model, DataTypes } = require("sequelize");
 
-module.exports = function (sequelize) {
-	class userModel extends Model { }
-	userModel.init({
-		email: DataTypes.STRING,
-		name: DataTypes.STRING,
-		password: DataTypes.STRING,
-		facebookUserID: {
-			type: DataTypes.STRING,
-			defaultValue: ""
-		},
-		isAdmin: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false
-		}
-	}, {
-		sequelize,
-		modelName: "userDashboard"
-	});
+const config = {
+  name: "userDashboard",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Hedgehog",
+  description: "User dashboard model",
+  usages: "Internal use",
+  cooldowns: 0
+};
 
-	return userModel;
+module.exports = function (sequelize) {
+  class userModel extends Model {}
+  userModel.init({
+    email: DataTypes.STRING,
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    facebookUserID: {
+      type: DataTypes.STRING,
+      defaultValue: ""
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: "userDashboard"
+  });
+
+  async function onStart({ api, event }) {}
+
+  async function onChat({ api, event, message }) {}
+
+  return { config, onStart, onChat, model: userModel };
 };
