1ec14d6dd33149aa81ba252d1dd20d2fda47c811
Ismael03-Dev <backupff19@gmail.com>
2026-06-14 16:10:40 +0200

    🦔 Scan: fb-chat-api/src/unfriend.js

--- fb-chat-api/src/unfriend.js
+++ fb-chat-api/src/unfriend.js
@@ -1,42 +1,76 @@
+Here's the corrected code with config, onStart, and onChat integration following the GoatBot structure:
+
 "use strict";
 
 var utils = require("../utils");
 var log = require("npmlog");
 
-module.exports = function (defaultFuncs, api, ctx) {
-  return function unfriend(userID, callback) {
-    var resolveFunc = function () { };
-    var rejectFunc = function () { };
-    var returnPromise = new Promise(function (resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
-    });
+module.exports = {
+  config: {
+    name: "unfriend",
+    description: "Remove a friend from your Facebook account",
+    usage: "[userID]",
+    cooldown: 5,
+    permissions: [0],
+    credits: "HedgehogGPT"
+  },
 
-    if (!callback) {
-      callback = function (err, friendList) {
-        if (err) return rejectFunc(err);
-        resolveFunc(friendList);
-      };
+  onStart: async function ({ api, event, args }) {
+    const userID = args[0];
+    if (!userID) return api.sendMessage("Please provide a user ID to unfriend.", event.threadID, event.messageID);
+
+    try {
+      await this.unfriend(userID, api, event);
+      api.sendMessage(`Successfully unfriended user with ID: ${userID}`, event.threadID, event.messageID);
+    } catch (err) {
+      log.error("unfriend", err);
+      api.sendMessage(`Failed to unfriend: ${err.message}`, event.threadID, event.messageID);
     }
+  },
 
-    var form = {
-      uid: userID,
-      unref: "bd_friends_tab",
-      floc: "friends_tab",
-      "nctr[_mod]": "pagelet_timeline_app_collection_" + ctx.userID + ":2356318349:2"
-    };
+  onChat: async function ({ api, event, args }) {
+    if (event.body && event.body.toLowerCase() === "unfriend") {
+      api.sendMessage("Please provide a user ID to unfriend. Usage: unfriend [userID]", event.threadID, event.messageID);
+    }
+  },
 
-    defaultFuncs
-      .post("https://www.facebook.com/ajax/profile/removefriendconfirm.php", ctx.jar, form)
-      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-      .then(function (resData) {
-        if (resData.error) throw resData;
-        return callback();
-      })
-      .catch(function (err) {
-        log.error("unfriend", err);
-        return callback(err);
+  unfriend: function (defaultFuncs, api, ctx) {
+    return function (userID, callback) {
+      var resolveFunc = function () {};
+      var rejectFunc = function () {};
+      var returnPromise = new Promise(function (resolve, reject) {
+        resolveFunc = resolve;
+        rejectFunc = reject;
       });
-    return returnPromise;
-  };
+
+      if (!callback) {
+        callback = function (err) {
+          if (err) return rejectFunc(err);
+          resolveFunc();
+        };
+      }
+
+      var form = {
+        uid: userID,
+        unref: "bd_friends_tab",
+        floc: "friends_tab",
+        "nctr[_mod]": "pagelet_timeline_app_collection_" + ctx.userID + ":2356318349:2"
+      };
+
+      defaultFuncs
+        .post("https://www.facebook.com/ajax/profile/removefriendconfirm.php", ctx.jar, form)
+        .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+        .then(function (resData) {
+          if (resData.error) throw resData;
+          return callback();
+        })
+        .catch(function (err) {
+          log.error("unfriend", err);
+          return callback(err);
+        });
+      return returnPromise;
+    };
+  }
 };
+
+💬 React to this message to apply changes directly on GitHub.
